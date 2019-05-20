2a6b1759741b565ad6b02508eec5623f12c5f942
Larry Botha <larry@fixate.it>
2019-05-20 22:30:03 +0200

    add sort examples

---
+++ src/02-sort-arrays-in-typescript.ts
@@ -0,0 +1,45 @@
+/*
+ * [].sort mutates the array
+ *
+ * To fix this, one can indicate that an array should not be nutated
+ */
+const arr: ReadonlyArray<string> = ['foo', 'bar'];
+
+// invalid
+// const copy = arr.sort()
+
+// valid
+const copy = arr.slice().sort();
+
+/*
+ * [].sort treats arrays as string[] by default
+ *
+ * Always provide a compare function when sorting anything other than string[
+ */
+const numArr: number[] = [1, 22, 3];
+console.log(numArr.slice().sort())
+
+const movies = [
+  {
+    name: 'Shawshank Redemption',
+    year: 1994,
+  },
+  {
+    name: 'The Godfather',
+    year: 1972,
+  },
+  {
+    name: 'The Godfather: Part II',
+    year:1974,
+  },
+  {
+    name: 'The Dark Knight',
+    year: 2008,
+  },
+];
+
+// sort ascending
+console.log(movies.slice().sort((b, a) => b.year - a.year > 0 ? 1 : -1))
+
+// sort descending
+console.log(movies.slice().sort((b, a) => a.year - b.year > 0 ? 1 : -1))
